69d4dfff3a7511868d30399e43667dd2c98724b6
nandhubala <44501307+nandhubala@users.noreply.github.com>
2019-12-31 16:40:47 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,82 @@
+package com.readygo;
+
+public class User {
+	private int userId;
+	private String fName;
+	private String lName;
+	private long contact;
+	private String email;
+	private String address;
+	private String city;
+	private String password;
+	private String cPassword;
+	private String image;
+	private boolean role;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public long getContact() {
+		return contact;
+	}
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getcPassword() {
+		return cPassword;
+	}
+	public void setcPassword(String cPassword) {
+		this.cPassword = cPassword;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public boolean isRole() {
+		return role;
+	}
+	public void setRole(boolean role) {
+		this.role = role;
+	}
+
+}
